5d726d8ac330ad57e32a6d7500abdf937f553b2e
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-21 08:23:17 +0100

    Aggiunti destroy, create, edit, store e update

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Post;
 
 class PostController extends Controller
 {
+    protected $validation = [
+        'title' => 'required|max:255',
+        'content' => 'required',
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -37,7 +43,24 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->validation);
+
+        $form_data = $request->all();
+
+        $slug = Str::slug($form_data['title']);
+        $count = 1;
+        while (Post::where('slug', $slug)->first()) {
+            $slug = Str::slug($form_data['title']) . "-" . $count;
+            $count++;
+        }
+
+        $form_data['slug'] = $slug;
+        $new_post = new Post();
+
+        $new_post->fill($form_data);
+        $new_post->save();
+
+        return redirect()->route('admin.posts.index');
     }
 
     /**
@@ -48,6 +71,10 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
+        if (!$post) {
+            abort(404);
+        }
+
         return view('admin.posts.show', compact('post'));
     }
 
@@ -57,9 +84,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
@@ -69,9 +96,26 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $request->validate($this->validation);
+
+        $form_data = $request->all();
+
+        if ($post->title == $form_data['title']) {
+            $slug = $post->slug;
+        } else {
+            $slug = Str::slug($form_data['title']);
+            $count = 1;
+            while (Post::where('slug', $slug)->where('id', '!=', $post->id)->first()) {
+                $slug = Str::slug($form_data['title']) . "-" . $count;
+                $count++;
+            }
+        }
+        $form_data['slug'] = $slug;
+
+        $post->update($form_data);
+        return redirect()->route('admin.posts.index');
     }
 
     /**
@@ -80,8 +124,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin.posts.index');
     }
 }
